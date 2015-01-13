76fa047de1028dcb724c99e30bb1b05c14d4a13e
SherlockZhong <sherlock.z0917@gmail.com>
2015-01-13 18:05:01 +0800

    added today widget

---
+++ DictionaryWidget/TodayViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TodayViewController.swift
+//  DictionaryWidget
+//
+//  Created by Sherlock Zhong on 1/10/15.
+//  Copyright (c) 2015 SherlockZhong. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+    
+    @IBOutlet var launchBtn: UIButton!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NewData)
+        
+    }
+    
+    @IBAction func launchAction(sender: UIButton) {
+        let url : NSURL = NSURL(string: "Dictionary://")!
+        self.extensionContext!.openURL(url, completionHandler: nil)
+    }
+    
+}
